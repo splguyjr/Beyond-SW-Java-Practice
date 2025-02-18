2317a28217376933742d708810e717472102f0a1
splguyjr <splguyjr@naver.com>
2025-02-18 15:55:13 +0900

    chapter3-level02-normal

---
+++ src/main/java/com/greedy/level02/normal/Application.java
@@ -0,0 +1,11 @@
+package com.greedy.level02.normal;
+
+public class Application {
+    public static void main(String[] args) {
+        RandomMaker randomMaker = new RandomMaker();
+        System.out.println(randomMaker.randomNumber(1, 2));
+        System.out.println(randomMaker.randomUpperAlphabet(10));
+        System.out.println(randomMaker.rockPaperScissors());
+        System.out.println(randomMaker.tossCoin());
+    }
+}
---
+++ src/main/java/com/greedy/level02/normal/RandomMaker.java
@@ -0,0 +1,37 @@
+package com.greedy.level02.normal;
+
+import java.util.Random;
+
+public class RandomMaker {
+
+    Random random = new Random();
+
+    public int randomNumber(int min, int max) {
+//        return (int)(Math.random() * (max - min + 1) + min );
+        return random.nextInt(min, max + 1);
+    }
+
+    public String randomUpperAlphabet(int length) {
+        String s = "";
+
+        for (int i = 0; i<length; i++) {
+            s+= (char)random.nextInt(65, 91);
+        }
+
+        return s;
+    }
+
+    public String rockPaperScissors() {
+        String[] s = {"가위", "바위", "보"};
+
+        int rand = random.nextInt(0,3);
+        return s[rand];
+    }
+
+    public String tossCoin() {
+        String[] s = {"앞면", "뒷면"};
+
+        int rand = random.nextInt(0,2);
+        return s[rand];
+    }
+}
