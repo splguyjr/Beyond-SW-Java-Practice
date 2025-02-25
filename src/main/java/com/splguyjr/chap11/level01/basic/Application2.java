ac939d8a09af42b89d7b33a634a144f4eec308bb
splguyjr <splguyjr@naver.com>
2025-02-25 16:17:13 +0900

    chap11-IO

---
+++ src/main/java/com/splguyjr/chap11/level01/basic/Application2.java
@@ -0,0 +1,53 @@
+package com.splguyjr.chap11.level01.basic;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Application2 {
+    public static void main(String[] args) {
+        /* ----- 입력 예시 -----
+         *
+         * (원본 파일이 존재 하는 경우)
+         * 원본 파일의 이름을 입력하세요 : origin.txt
+         * 복사 파일의 이름을 입력하세요 : copy.txt
+         *
+         * ----- 출력 예시 -----
+         *
+         * 파일 복사가 성공적으로 완료 되었습니다.
+         *
+         * ----- 입력 예시 -----
+         *
+         * (원본 파일이 존재하지 않는 경우)
+         * 원본 파일의 이름을 입력하세요 : origin2.txt
+         * 복사 파일의 이름을 입력하세요 : copy2.txt
+         *
+         * ----- 출력 예시 -----
+         *
+         * 오류 : origin2.txt (지정된 파일을 찾을 수 없습니다)
+         *
+         */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("원본 파일의 이름을 입력하세요 :");
+        String src = sc.next();
+        sc.nextLine();
+        System.out.print("복사 파일의 이름을 입력하세요 :");
+        String dst = sc.next();
+
+        try (FileReader fr = new FileReader(src); FileWriter fw = new FileWriter(dst)) {
+            int value;
+            while((value = fr.read()) != -1) {
+                fw.write((char)(value));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("오류 : " + src + " (지정된 파일을 찾을 수 없습니다)");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
